ce82fc745d7633a8f71c6eb94aa5da9c5467891b
Rachel Cohen <cohenr7@uw.edu>
2018-05-15 13:52:38 -0700

    added comments on what I am doing in the steps

--- script/week8_assignment.R
+++ script/week8_assignment.R
@@ -1,8 +1,11 @@
+#load the packages needed
 library(dplyr)
 library(ggplot2)
 
+#read in the data
 reef_fish <- read.csv('data/RLSreeffishdataset.csv')
 View(reef_fish)
 
+#start looking for a trend in data
 ggplot(reef_fish, aes(Depth)) +
   geom_histogram()
