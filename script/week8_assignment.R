507b7e80c54a8c28f3678464687c96fa623972f4
raegan13 <38798434+raegan13@users.noreply.github.com>
2018-05-17 00:56:07 -0700

    Update week8_assignment.R

--- script/week8_assignment.R
+++ script/week8_assignment.R
@@ -9,3 +9,10 @@ View(reef_fish)
 #start looking for a trend in data
 ggplot(reef_fish, aes(Depth)) +
   geom_histogram()
+
+# If you want to explore diver bias this might be an interesting graph to try?
+# Maybe look at just one site code at a time for this to be effective? Use dplyr
+
+ggplot(reef_fish) +
+geom_point(aes(x = Diver, y = Total)
+facet_wrap(~Family)
